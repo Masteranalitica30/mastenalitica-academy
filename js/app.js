98d9fdba533a39a0de26577a317099a352c27452
Masteranalitica30 <ceo@masteranalitica.lat>
2026-02-24 16:52:33 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+fetch("data/cursos.json")
+.then(res=>res.json())
+.then(data=>{
 
+let html="";
+
+data.cursos.forEach(curso=>{
+html+=`
+<div>
+<h2>${curso.nombre}</h2>
+<p>Inicio: ${curso.inicio}</p>
+<p>${curso.horas} horas</p>
+</div>
+`;
+});
+
+document.getElementById("listaCursos").innerHTML=html;
+
+});
